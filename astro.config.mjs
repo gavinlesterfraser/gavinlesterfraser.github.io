47165eee7c94acc7587b35a17d9dd9a3c06d8e79
gavinlesterfraser <gavinlesterfraser@gmail.com>
2025-01-23 19:45:46 -0800

    fixed dev errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro';
-import sharp from '@astrojs/sharp';
+// @ts-check
+import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [sharp()],
+  integrations: [preact()]
 });
