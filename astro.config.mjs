46cae2992433cec9c0e66e1168ebb5fa974c7ab4
gavinlesterfraser <gavinlesterfraser@gmail.com>
2025-02-08 17:45:03 -0800

    trying again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: netlify()
 });
